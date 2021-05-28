eabe7cbbab68a17701f515ebf5b56ba58e59d6d8
Shradha Baldawa <shradha.baldawa@jadeglobal.com>
2021-05-28 10:47:11 +0530

    controller changes

--- src/main/java/com/googledrive/api/controller/GoogleDriveController.java
+++ src/main/java/com/googledrive/api/controller/GoogleDriveController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,8 @@ import com.googledrive.api.service.GoogleDriveService;
  * @author swathi.kompella
  *
  */
+
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class GoogleDriveController {
 
@@ -65,8 +68,9 @@ public class GoogleDriveController {
 	
 
 	@GetMapping("/downloadSource")
-	public void downloadSource() {
+	public ResponseEntity<String> downloadSource() {
 		service.downloadSource();
+		return ResponseEntity.ok("Source Code Downloaded Successfully....Please check your drive");
 	}
 
 	@PostMapping(value = "/upload",consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE} )
